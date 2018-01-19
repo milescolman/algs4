138820c449b1486076f8d4b2e4c894d076db80af
Miles Colman <mcolman@gmail.com>
2018-01-18 21:02:21 -0800

    all Board API methods implemented and tested

--- 8puzzle/Board.java
+++ 8puzzle/Board.java
@@ -1,19 +1,28 @@
+import java.util.Iterator;
 import java.lang.Math;
 import edu.princeton.cs.algs4.StdRandom;
-
+import java.util.Stack;
 public class Board {
     private int[][] blocks = null;
     private int manhattanDist = 0;
-    private int moves = 0;
+    private int hammingDist = 0;
     public Board(int[][] blocks) {          // construct a board from an n-by-n array of blocks
                                            // (where blocks[i][j] = block in row i, column j)
         if (blocks == null) throw new java.lang.IllegalArgumentException();
-        this.blocks = blocks;
+        // defensive copy
+        int n = blocks.length;
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                arr[i][j] = blocks[i][j];
+        this.blocks = arr;
+        manhattanDist = manhattanCalc();
+        hammingDist = hammingCalc();
     }
     public int dimension() {                // board dimension n
         return this.blocks.length;
     }
-    public int hamming() {                   // number of blocks out of place
+    private int hammingCalc() {                   // number of blocks out of place
         int n = dimension();
         int hamming = 0;
         for (int i = 0; i < n; i++) {
@@ -23,12 +32,14 @@ public class Board {
             }
         }
         hamming--; // for zero block
-        return hamming + moves;
+        return hamming;
+    }
+    public int hamming() {
+        return hammingDist;
     }
         
     public int manhattan() {                 // sum of Manhattan distances between blocks and goal
-        manhattanDist = manhattanCalc();
-        return manhattanDist + moves;
+        return manhattanDist;
     }
     private int manhattanCalc() {
         int manhattan = 0;
@@ -41,7 +52,7 @@ public class Board {
                         continue;
                     int Vdistance = Math.abs(((val - 1) % n) - j);
                     int Hdistance = Math.abs((val - 1) / n - i);
-                    System.out.println(val + " " + (Vdistance + Hdistance));
+                    //System.out.println(val + " " + (Vdistance + Hdistance));
                     manhattan += Vdistance + Hdistance;                   
                 }
             }
@@ -77,10 +88,63 @@ public class Board {
         
         return new Board(arr);
     }
-//    public boolean equals(Object y)        // does this board equal y?
-//    public Iterable<Board> neighbors()     // all neighboring boards
-//    public String toString()               // string representation of this board (in the output format specified below)
-
+    public boolean equals(Object y) {       // does this board equal y?
+        if (!(y instanceof Board)) return false;
+        int n = dimension();
+        int nY = ((Board) y).blocks.length;
+        if (n != nY) return false;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (blocks[i][j] != ((Board) y).blocks[i][j])
+                    return false;
+        return true;
+    }
+    private void addNeighbor(Stack<Board> s, int i, int j, int iOffset, int jOffset) {
+        blocks[i][j] = blocks[i+iOffset][j+jOffset];
+        blocks[i+iOffset][j+jOffset] = 0;
+        //create new Board, add to list
+        Board b = new Board(blocks);
+        s.push(b);
+        //undo swap
+        blocks[i+iOffset][j+jOffset] = blocks[i][j];
+        blocks[i][j] = 0;
+    }
+    public Iterable<Board> neighbors() {     // all neighboring boards
+        Stack<Board> boards = new Stack<Board>(); 
+        int n = dimension();
+        // find 0 location
+        int i;
+        int j = 0;
+        rowLoop:
+        for (i = 0; i < n; i++)
+            for (j = 0; j < n; j++)
+                if (blocks[i][j] == 0)
+                    break rowLoop;
+        if (i != 0)  // create Board with swapping with element above
+            addNeighbor(boards, i, j, -1, 0);
+        if (i != n - 1) { //create Board swapping element below
+            addNeighbor(boards, i, j, +1, 0);
+        }
+        if (j != 0) { // create Board with swapping element to left
+            addNeighbor(boards, i, j, 0, -1);
+        }
+        if (j != n - 1) { // create Board swapping element to right
+            addNeighbor(boards, i, j, 0, +1);
+        }             
+        return boards;
+    }
+        
+    public String toString() {              // string representation of this board (in the output format specified below)
+        int n = dimension();
+        String str = n + "\n";
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                str += " " + blocks[i][j] + " ";
+            str+= "\n";
+        }
+        str += "\n";
+        return str;
+    }
     public static void main(String[] args) { // unit tests (not graded)
         int[][] arr = new int[][] {
                 new int[] {8,1,3},
@@ -88,14 +152,24 @@ public class Board {
                 new int[] {7,6,5},
         };
         Board b = new Board(arr);
+        System.out.println("hamming distance: " + b.hamming());
         System.out.println("manhattan distance: " + b.manhattan());
         System.out.println("is solved?: " + b.isGoal());
         int[][] arr2 = new int[][] {
-                new int[] {1,2,3},
-                new int[] {4,5,6},
-                new int[] {7,8,0},
+                new int[] {0,2,3},
+                new int[] {4,1,6},
+                new int[] {7,8,5},
         };
         Board b2 = new Board(arr2);
         Board b3 = b2.twin();
+        Board b4 = new Board(arr2);
+        System.out.println("b2 equals b3? " + b2.equals(b3));
+        System.out.println("b2 equals b4? " + b2.equals(b4));
+        System.out.print(b2.toString());
+        System.out.print(b4.toString());
+        System.out.println("***** Neighbors test. starting position: *****");
+        Iterable<Board> neighbors = b4.neighbors();
+        for (Board bb : neighbors)
+            System.out.println(bb.toString());
     }
 }
